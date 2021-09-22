be71b3e241a400bc15ca67452780515c4fc14f23
John Silva <jhonmvl18@gmail.com>
2021-09-22 16:26:35 -0500

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
-import '@Styles/App.scss';
+import "./assets/styles/app.scss"
 
 import { Home } from '@Pages';
 import { GameFactory } from '@Elements';
